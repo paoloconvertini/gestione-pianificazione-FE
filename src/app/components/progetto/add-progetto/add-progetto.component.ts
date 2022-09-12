bfa1766a21805ef6a520dae27c0569eae1a4d075
paolo <paolo.convertini@gmial.com>
2022-09-12 15:24:56 +0200

    add progetto

--- src/app/components/progetto/add-progetto/add-progetto.component.ts
+++ src/app/components/progetto/add-progetto/add-progetto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ProgettoService} from "../../../services/progetto/progetto.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Progetto} from "../../../models/progetto";
 
 @Component({
   selector: 'app-add-progetto',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-progetto.component.css']
 })
 export class AddProgettoComponent implements OnInit {
+  progetto: Progetto = {
+    nome: ''
+  }
+  message: string = '';
 
-  constructor() { }
+  constructor(private service: ProgettoService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.message = '';
+  }
+
+  saveProgetto(): void {
+    const data = {
+      nome: this.progetto.nome,
+    };
+    this.service.create(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          if(res && !res.error) {
+            this.openSnackBar();
+          }
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  openSnackBar() {
+    this._snackBar.open('Progetto salvato con successo!!', '', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      duration: 2000
+    });
   }
 
 }
