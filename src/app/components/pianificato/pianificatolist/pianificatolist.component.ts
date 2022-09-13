5abb36e11de2c16c9c999220ea6d11e082d71b44
paolo <paolo.convertini@gmial.com>
2022-09-13 10:22:54 +0200

    add progetto

--- src/app/components/pianificato/pianificatolist/pianificatolist.component.ts
+++ src/app/components/pianificato/pianificatolist/pianificatolist.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {Dipendente} from "../../../models/dipendente";
+import {Pianificato} from "../../../models/pianificato";
+import {MatPaginator} from "@angular/material/paginator";
+import {DipendenteService} from "../../../services/dipendente/dipendente.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-pianificatolist',
@@ -6,10 +13,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pianificatolist.component.css']
 })
 export class PianificatolistComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
+    'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']
+  dataSource = new MatTableDataSource<Pianificato>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  loader = false;
+  constructor(private service: DipendenteService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/models/pianificato.ts
+++ src/app/models/pianificato.ts
@@ -1,2 +1,21 @@
+import {Dipendente} from "./dipendente";
+import {Progetto} from "./progetto";
+
 export class Pianificato {
+  id?:any
+  gennaio?:string
+  febbraio?:string
+  marzo?:string
+  aprile?:string
+  maggio?:string
+  giugno?:string
+  luglio?:string
+  agosto?:string
+  settembre?:string
+  ottobre?:string
+  novembre?:string
+  dicembre?:string
+  flagPianificato?:boolean
+  dipendente?:Dipendente
+  progetto?:Progetto
 }
